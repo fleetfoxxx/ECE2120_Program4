44aec7dbacfc8a4d3379d5fe92aeb17b8d6595fb
cbthomas94 <charlesbthomas94@gmail.com>
2015-12-09 13:37:27 -0600

    Driver Skeleton

--- RestaurantGUI.java
+++ RestaurantGUI.java
@@ -1,7 +1,17 @@
-/*
+/**
  *  This is going to be the GUI driver.
  *
  *
- *
- *
+ * @author Charlie Thomas and Timothy Porter 
+ * @date Dec 10, 2015
  */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class RestaurantGUI {
+	public static void main(String[] args){
+
+	}
+}
