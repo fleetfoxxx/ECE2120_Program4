eed8fec2d8811808df589c9cd2af2c09b1348532
Timothy Porter <ymmit987@gmail.com>
2015-12-09 13:51:17 -0600

    Renamed Utilities to utilities and filled in some necessary GUI elements

--- RestaurantGUI.java
+++ RestaurantGUI.java
@@ -10,8 +10,28 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class RestaurantGUI {
-	public static void main(String[] args){
+public class RestaurantGUI extends CenterFrame implements ActionListener {
 
+	public static void main(String[] args){
+      RestaurantGUI rg = new RestaurantGUI(800, 480);
 	}
+
+   public RestaurantGUI(int width, int height) {
+      super(width, height, "8====D~ (. Y .)");
+
+      setResizable(false);
+      setUp(width, height);
+      setVisible(true);
+   }
+
+   private void setUp(int width, int height) {
+      setBackground(Color.white);
+
+      // setup GUI elements
+   }
+
+   // actionPerformed definition for ActionListener interface
+   public void actionPerformed(ActionEvent ae) {
+      // TODO
+   }  
 }
