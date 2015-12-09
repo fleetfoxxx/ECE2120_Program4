02b56392a61a34acb04d3a0753d877c0bc902ce7
Timothy Porter <ymmit987@gmail.com>
2015-12-09 16:24:04 -0600

    addid such J component 2 penel

--- RestaurantGUI.java
+++ RestaurantGUI.java
@@ -31,8 +31,6 @@ public class RestaurantGUI extends CenterFrame implements ActionListener {
       RestaurantGUI rg = new RestaurantGUI(800, 480, name, fileName, isObject);
 	}
 
-   private JPanel windowPanel;
-   private JPanel topPanel;
    private JPanel inputPanel;
    private JPanel buttonPanel;
 
@@ -80,10 +78,11 @@ public class RestaurantGUI extends CenterFrame implements ActionListener {
 
    private JComboBox categoryCB;
 
-   private JRadioButton allItemsRB;
-   private JRadioButton wholesalePriceRB;
-   private JRadioButton objectFileRB;
+   private JCheckBox allItemsCB;
+   private JCheckBox wholesalePriceCB;
+   private JCheckBox objectFileCB;
 
+   private JTextArea outputTextArea;
    private JScrollPane outputScrollPane;
 
    public RestaurantGUI(int width, int height, String name, String fileName, Boolean isObject) {
@@ -91,14 +90,14 @@ public class RestaurantGUI extends CenterFrame implements ActionListener {
       super(width, height, name);
       // Create Restaurant Object to manipulate!
       try {
-         Restaurant theRestaurant =new Restaurant(name, fileName, isObject);
+         Restaurant theRestaurant = new Restaurant(name, fileName, isObject);
       } catch (RestaurantException re) {
          System.out.println(re.getMessage());
          System.out.println("Problem creating Restaurant - exiting program.");
          return;
       }
 
-      setResizable(false);
+      setResizable(true);
       setUp(width, height);
       setVisible(true);
    }
@@ -107,21 +106,139 @@ public class RestaurantGUI extends CenterFrame implements ActionListener {
       setBackground(Color.white);
 
       // setup GUI elements
+      buttonPanel = new JPanel();
+      add(buttonPanel, BorderLayout.SOUTH);
 
-      // panel
-      windowPanel = new JPanel();
-      windowPanel.setBackground(Color.pink);
-      windowPanel.setLayout(new GridLayout(2, 1));
-      add(windowPanel);
+      inputPanel = new JPanel();
+      add(inputPanel, BorderLayout.WEST);
 
-      topPanel = new JPanel();
-      topPanel.setBackground(Color.green);
-      topPanel.setLayout(new GridLayout(1, 2));
-      windowPanel.add(topPanel);
+      // input panel
+      EasyGridBag inputPanelBag = new EasyGridBag(15, 2, inputPanel);
+      inputPanelBag.setColumnWeight(1, 1.0);
+      inputPanelBag.setColumnWeight(2, 10.0);
+      inputPanel.setLayout(inputPanelBag);
 
-      
+      reviewerNameLbl = new JLabel("Reviewer Name");
+      reviewerRatingLbl = new JLabel("Reviewer Rating");
+      reviewDateLbl = new JLabel("Review Date");
+      itemNameLbl = new JLabel("Item Name");
+      numOrdersLbl = new JLabel("# Orders");
+      categoryLbl = new JLabel("Category");
+      servingSizeLbl = new JLabel("Serving Size");
+      numCaloriesLbl = new JLabel("# Calories");
+      retailPriceLbl = new JLabel("Retail Price");
+      wholesalePriceLbl = new JLabel("Wholesale Price");
+      priceChangeLbl = new JLabel("% Price Change");
+      sortFieldLbl = new JLabel("Sort Field");
+      sortAlgorithmLbl = new JLabel("Sort Algorithm");
 
+      reviewerName = new JTextField();
+      reviewerName.setPreferredSize(new Dimension(150, 24));
+      reviewerRating = new JTextField();
+      reviewerRating.setPreferredSize(new Dimension(150, 24));
+      reviewDate = new JTextField();
+      reviewDate.setPreferredSize(new Dimension(150, 24));
+      itemName = new JTextField();
+      itemName.setPreferredSize(new Dimension(150, 24));
+      numOrders = new JTextField();
+      numOrders.setPreferredSize(new Dimension(150, 24));
+      servingSize = new JTextField();
+      servingSize.setPreferredSize(new Dimension(150, 24));
+      numCalories = new JTextField();
+      numCalories.setPreferredSize(new Dimension(150, 24));
+      retailPrice = new JTextField();
+      retailPrice.setPreferredSize(new Dimension(150, 24));
+      wholesalePrice = new JTextField();
+      wholesalePrice.setPreferredSize(new Dimension(150, 24));
+      priceChange = new JTextField();
+      priceChange.setPreferredSize(new Dimension(150, 24));
+      sortField = new JTextField();
+      sortField.setPreferredSize(new Dimension(150, 24));
+      sortAlgorithm = new JTextField();
+      sortAlgorithm.setPreferredSize(new Dimension(150, 24));
 
+      categoryCB = new JComboBox();
+      categoryCB.setPreferredSize(new Dimension(150, 24));
+
+      allItemsCB = new JCheckBox();
+      wholesalePriceCB = new JCheckBox();
+      objectFileCB = new JCheckBox();
+
+      inputPanelBag.fillCellAlignWithinCell(1, 1, GridBagConstraints.WEST, reviewerNameLbl);
+      inputPanelBag.fillCellAlignWithinCell(2, 1, GridBagConstraints.WEST, reviewerRatingLbl);
+      inputPanelBag.fillCellAlignWithinCell(3, 1, GridBagConstraints.WEST, reviewDateLbl);
+      inputPanelBag.fillCellAlignWithinCell(4, 1, GridBagConstraints.WEST, itemNameLbl);
+      inputPanelBag.fillCellAlignWithinCell(5, 1, GridBagConstraints.WEST, numOrdersLbl);
+      inputPanelBag.fillCellAlignWithinCell(6, 1, GridBagConstraints.WEST, categoryLbl);
+      inputPanelBag.fillCellAlignWithinCell(7, 1, GridBagConstraints.WEST, servingSizeLbl);
+      inputPanelBag.fillCellAlignWithinCell(8, 1, GridBagConstraints.WEST, numCaloriesLbl);
+      inputPanelBag.fillCellAlignWithinCell(9, 1, GridBagConstraints.WEST, retailPriceLbl);
+      inputPanelBag.fillCellAlignWithinCell(10, 1, GridBagConstraints.WEST, wholesalePriceLbl);
+      inputPanelBag.fillCellAlignWithinCell(11, 1, GridBagConstraints.WEST, priceChangeLbl);
+      inputPanelBag.fillCellAlignWithinCell(12, 1, GridBagConstraints.WEST, sortFieldLbl);
+      inputPanelBag.fillCellAlignWithinCell(13, 1, GridBagConstraints.WEST, sortAlgorithmLbl);
+
+      inputPanelBag.fillCellAlignWithinCell(1, 2, GridBagConstraints.WEST, reviewerName);
+      inputPanelBag.fillCellAlignWithinCell(2, 2, GridBagConstraints.WEST, reviewerRating);
+      inputPanelBag.fillCellAlignWithinCell(3, 2, GridBagConstraints.WEST, reviewDate);
+      inputPanelBag.fillCellAlignWithinCell(4, 2, GridBagConstraints.WEST, itemName);
+      inputPanelBag.fillCellAlignWithinCell(5, 2, GridBagConstraints.WEST, numOrders);
+      inputPanelBag.fillCellAlignWithinCell(6, 2, GridBagConstraints.WEST, categoryCB);
+      inputPanelBag.fillCellAlignWithinCell(7, 2, GridBagConstraints.WEST, servingSize);
+      inputPanelBag.fillCellAlignWithinCell(8, 2, GridBagConstraints.WEST, numCalories);
+      inputPanelBag.fillCellAlignWithinCell(9, 2, GridBagConstraints.WEST, retailPrice);
+      inputPanelBag.fillCellAlignWithinCell(10, 2, GridBagConstraints.WEST, wholesalePrice);
+      inputPanelBag.fillCellAlignWithinCell(11, 2, GridBagConstraints.WEST, priceChange);
+      inputPanelBag.fillCellAlignWithinCell(12, 2, GridBagConstraints.WEST, sortField);
+      inputPanelBag.fillCellAlignWithinCell(13, 2, GridBagConstraints.WEST, sortAlgorithm);
+
+      // inputPanelBag.fillCellAlignWithinCell(14, 1, GridBagConstraints.WEST, allItemsRB);
+      // inputPanelBag.fillCellAlignWithinCell(14, 2, GridBagConstraints.WEST, allItemsRB);
+      // inputPanelBag.fillCellAlignWithinCell(15, 1, GridBagConstraints.WEST, allItemsRB);
+
+      // scroll panel
+      outputTextArea = new JTextArea(30, 75);
+      outputTextArea.setEditable(false);
+      JScrollPane outputScrollPane = new JScrollPane(outputTextArea);
+
+      outputScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+      add(outputScrollPane, BorderLayout.EAST);
+
+      // button panel
+      EasyGridBag buttonPanelBag = new EasyGridBag(1, 15, buttonPanel);
+      buttonPanel.setLayout(buttonPanelBag);
+
+      statusBtn = new JButton("Status");
+      allItemNamesBtn = new JButton("All Item Names");
+      sortBtn = new JButton("Sort");
+      helpBtn = new JButton("Help");
+      addBtn = new JButton("+");
+      removeBtn = new JButton("-");
+      activateBtn = new JButton("Activate");
+      discontinueBtn = new JButton("Discontinue");
+      orderBtn = new JButton("Order");
+      rateItemBtn = new JButton("Raise Item");
+      updatePriceBtn = new JButton("Update Price");
+      profitBtn = new JButton("Profit");
+      avgRatingBtn = new JButton("Average Rating");
+      writeFileBtn = new JButton("Write File");
+
+      buttonPanelBag.fillCellCenterWithinCell(1, 1, statusBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 2, allItemNamesBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 3, sortBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 4, helpBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 5, addBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 6, removeBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 7, activateBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 8, discontinueBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 9, orderBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 10, rateItemBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 11, updatePriceBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 12, profitBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 13, avgRatingBtn);
+      buttonPanelBag.fillCellCenterWithinCell(1, 14, writeFileBtn);
+
+      pack();
    }
 
    // actionPerformed definition for ActionListener interface
